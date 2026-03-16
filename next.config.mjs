ce919bcb0c4bf62171cbab11b13e01770cdb0ff6
overwaterbz <rick@overwaterbz.com>
2026-03-16 17:04:15 -0600

    feat: add bundle analyzer, GA4 component

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const analyzer = withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -34,4 +38,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default analyzer(nextConfig);
